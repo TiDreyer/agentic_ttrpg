d6cf994d4e0c1c2ff9d7231f940d6a8aac54cfc8
Timo Dreyer <git@timodreyer.de>
2026-06-13 17:56:58 +0200

    Add a first tool for rolling dice

--- agentic_ttrpg/__main__.py
+++ agentic_ttrpg/__main__.py
@@ -1,8 +1,8 @@
 from pydantic_ai import Agent
 
-from agentic_ttrpg import config, prompts
+from agentic_ttrpg import config, prompts, tools
 
-game_master = Agent(model=config.MODEL, system_prompt=prompts.GM_SYSTEM)
+game_master = Agent(model=config.MODEL, system_prompt=prompts.GM_SYSTEM, tools=[tools.roll_dice])
 
-result = game_master.run_sync("The game starts, introduce yourself to the players.")
+result = game_master.run_sync("The game starts with a battle scene. Roll dice to see how many enemies are present and introduce the situation to the players")
 print(result.output)
---
+++ agentic_ttrpg/tools.py
@@ -0,0 +1,16 @@
+from random import randint
+from typing import Literal
+
+from pydantic_ai import Tool
+
+
+def _roll_dice(type_of_dice: Literal[2, 4, 6, 8, 10, 12, 20, 100] = 20, number_of_dice: int = 1) -> str:
+    """Roll n dice of a given type and return the individual results as well as the sum"""
+    if number_of_dice < 1:
+        return "You can only roll a number of dice greater than zero"
+    results = [randint(1, type_of_dice) for _ in range(number_of_dice)]
+    return_str = f"sum({results}) = {sum(results)}"
+    return return_str
+
+
+roll_dice = Tool(_roll_dice, takes_ctx=False)
