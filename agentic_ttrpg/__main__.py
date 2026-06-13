0e28ba894a8ae9bfe4bb75681047100893af0d2e
Timo Dreyer <git@timodreyer.de>
2026-06-13 18:00:05 +0200

    Print all messages of the agent to understand run time logic

--- agentic_ttrpg/__main__.py
+++ agentic_ttrpg/__main__.py
@@ -6,3 +6,9 @@ game_master = Agent(model=config.MODEL, system_prompt=prompts.GM_SYSTEM, tools=[
 
 result = game_master.run_sync("The game starts with a battle scene. Roll dice to see how many enemies are present and introduce the situation to the players")
 print(result.output)
+
+print("\n" + 80 * "=")
+
+for message in result.all_messages():
+    print(repr(message))
+    print(80 * "-")
