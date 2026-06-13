29cac19a322d4ccecec2a8c471ff327933f2ff79
Timo Dreyer <git@timodreyer.de>
2026-06-13 21:01:21 +0200

    Introduce more randomness in player order

--- agentic_ttrpg/__main__.py
+++ agentic_ttrpg/__main__.py
@@ -1,28 +1,41 @@
+from random import choice
+
 from agentic_ttrpg import players, prompts, table_conversation
 
-__MAX_ROUNDS = 5
+__MAX_TURNS = 20
+__DEFAULT_TABLE_LOG_LENGTH = 20
 __ACTIVE_PLAYERS = [
     players.CHLOE,
     players.JULES,
     players.MAYA,
     players.PRIYA,
     players.SULLY,
-    players.MARCUS,
 ]
+# give GM double chance of being called upon
+__RANDOM_SET = __ACTIVE_PLAYERS + [players.MARCUS, players.MARCUS]
+
+
+def __call_upon_player(player: players.Agent) -> None:
+    prompt = (
+        "These are the last things that happened at the table:\n"
+        + table_conversation.transcript_record(__DEFAULT_TABLE_LOG_LENGTH)
+        + prompts.INTERACTION_OPTION
+    )
+    player.run_sync(prompt)
 
 
 def main() -> None:
+    # start and introduction round
     players.MARCUS.run_sync(prompts.MODERATION_START)
-    for i in range(__MAX_ROUNDS):
-        print(f"[SYSTEM] Round {i}")
-        for player in __ACTIVE_PLAYERS:
-            print(f"[SYSTEM] active: {player}")
-            prompt = (
-                "These are the last things that happened at the table:\n"
-                + table_conversation.transcript_record(len(__ACTIVE_PLAYERS))
-                + prompts.INTERACTION_OPTION
-            )
-            player.run_sync(prompt)
+    for player in __ACTIVE_PLAYERS:
+        __call_upon_player(player=player)
+
+    # main game loop - randomly switch between players
+    for i in range(__MAX_TURNS):
+        player = choice(__RANDOM_SET)
+        __call_upon_player(player=player)
+
+    # end of the game
     players.MARCUS.run_sync(prompts.MODERATION_END)
 
 
